cf41cf65a2060c384993c243989b0406624dfc78
Oleksandr Yakymenko <56623225+AlexKaihatsusha@users.noreply.github.com>
2023-10-20 23:15:45 +0200

    Added highlight tile logic
    Added TileHighlightManager
    - Keeps tile highlight materials
    - Has two functions to highlight the tile when Hover and when Select
    Added GridManager
    - Keep a reference to TileHighlightManager
    - Has two functions to Use TileHighlightManager logic
    Modified MovePlayer class logic:
    - Added reference variable to gridManager
    - Ray and mousePosition were moved upper in the Update function
    - Added else if for highlighting the tile when just Hover
    - Added to press event highlighting the tile when Select

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class MovePlayer : MonoBehaviour
@@ -8,7 +9,10 @@ public class MovePlayer : MonoBehaviour
     private Vector3 targetPosition;
     private Vector3 currentPosition;
     private string tileName;
-
+    
+    //for highlighting
+    public GridManager gridManager;
+    private RaycastHit rayHit;
     private void Awake()
     {
         cameraVar = Camera.main;
@@ -17,19 +21,27 @@ public class MovePlayer : MonoBehaviour
 
     private void Update()
     {
+        
         Mouse mouse = Mouse.current;
+        Vector3 mousePosition = mouse.position.ReadValue();
+        Ray ray = cameraVar.ScreenPointToRay(mousePosition);
         if (mouse.leftButton.wasPressedThisFrame)
         {
-            Vector3 mousePosition = mouse.position.ReadValue();
-            Ray ray = cameraVar.ScreenPointToRay(mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
+                //highlight selected tile
+                gridManager.HandleMouseSelectedTile(rayHit.transform);
+                
                 targetPosition = new Vector3(hit.transform.position.x, 1, hit.transform.position.z);
                 tileName = hit.transform.name;
                 Debug.Log("Tile selected: " + tileName);
             }
         }
-
+        else if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out rayHit))
+        {
+            //highlight the tile when just hovering
+            gridManager.HandleMouseOverTile(rayHit.transform);
+        }
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
         currentPosition = targetPosition;
     }
