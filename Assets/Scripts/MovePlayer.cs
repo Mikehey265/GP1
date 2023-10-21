634345f5f4b605ab637518ca5ce08c7140ee39b9
Oleksandr Yakymenko <56623225+AlexKaihatsusha@users.noreply.github.com>
2023-10-21 13:13:31 +0200

    Update code in MovePlayer

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -10,9 +10,6 @@ public class MovePlayer : MonoBehaviour
     private Vector3 currentPosition;
     private string tileName;
     
-    //for highlighting
-    public GridManager gridManager;
-    private RaycastHit rayHit;
     private void Awake()
     {
         cameraVar = Camera.main;
@@ -23,24 +20,16 @@ public class MovePlayer : MonoBehaviour
     {
         
         Mouse mouse = Mouse.current;
-        Vector3 mousePosition = mouse.position.ReadValue();
-        Ray ray = cameraVar.ScreenPointToRay(mousePosition);
         if (mouse.leftButton.wasPressedThisFrame)
         {
+            Vector3 mousePosition = mouse.position.ReadValue();
+            Ray ray = cameraVar.ScreenPointToRay(mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                //highlight selected tile
-                gridManager.HandleMouseSelectedTile(rayHit.transform);
-                
                 targetPosition = new Vector3(hit.transform.position.x, 1, hit.transform.position.z);
                 tileName = hit.transform.name;
                 Debug.Log("Tile selected: " + tileName);
             }
-        }
-        else if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out rayHit))
-        {
-            //highlight the tile when just hovering
-            gridManager.HandleMouseOverTile(rayHit.transform);
         }
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
         currentPosition = targetPosition;
